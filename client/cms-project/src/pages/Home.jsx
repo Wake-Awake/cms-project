b56c576758373c18757ea51951206a360515c8f1
SATHYA NARAYANAN K R <162536966+Wake-Awake@users.noreply.github.com>
2025-05-30 03:53:16 +0000

    changes

--- client/cms-project/src/pages/Home.jsx
+++ client/cms-project/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-import Signin from "./Signin";
-import Signup from "./SignUp";
+
 
 export default function Home() {
   return (
--- client/cms-project/src/routes/AppRoutes.jsx
+++ client/cms-project/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import SignUp from "../pages/SignUp";
+
 import Signin from "../pages/Signin";
 
 
@@ -9,7 +9,7 @@ export default function AppRoutes() {
     <Routes>
         <Route index element={<Home />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/Sign-up" element={<SignUp/>}/>
+   
         <Route path="/Sign-in" element={<Signin />} />
     </Routes>
   )
